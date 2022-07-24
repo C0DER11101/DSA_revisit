20857266d87141df313a4233fc6f228e7dabe327
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-07-24 12:58:28 +0530

    header file containing declarations

---
+++ ll2.h
@@ -0,0 +1,22 @@
+#ifndef LL_H
+#define LL_H
+
+#include<stdbool.h>
+struct node
+{
+	int data;
+	struct node*link;
+};
+
+typedef struct node node;
+
+node*insert(int, node*);
+node*del(int, node*);
+node*revlist(node*);
+void disprev(node*);
+int count(node*);
+bool search(int, node*);
+void terminate(node*);
+
+
+#endif

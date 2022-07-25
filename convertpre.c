a88da378950e49f0f48545b1001420f4ea70ac86
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-07-25 22:34:55 +0530

    .c file to convert infix to prefix expression

---
+++ convertpre.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define FLAG 1
+#include "stackexp.h"
+#include "intopre.h"
+
+
+int main(void)
+{
+	char*infix;
+
+	infix=(char*)malloc(100*sizeof(char));
+
+	printf("enter infix expression: ");
+	scanf("%s", infix);
+
+	char*revexp;
+	int idx=-1;
+
+	revexp=(char*)malloc(100*sizeof(char));
+
+	for(int i=strlen(infix)-1; i>=0; i--)
+	{
+		if(infix[i]==')')
+		{
+			idx++;
+			revexp[idx]='(';
+		}
+
+		else if(infix[i]=='(')
+		{
+			idx++;
+			revexp[idx]=')';
+		}
+
+		else
+		{
+			idx++;
+			revexp[idx]=infix[i];
+		}
+
+	}
+
+
+	scanexp(revexp);
+
+	free(infix);
+	free(revexp);
+	printf("\n\n\n");
+
+	return 0;
+}

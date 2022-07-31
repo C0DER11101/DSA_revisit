12536aca9bb655e46a23475a8c46645b4a741ed0
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-07-31 20:42:03 +0530

    recursive fibonacci

--- fibo.c
+++ fibo.c
@@ -3,5 +3,13 @@
 
 int main(void)
 {
+	int n;
+	printf("\n\nhow many elements do you want to print in fibonacci series? ");
+	scanf("%d", &n);
+
+	for(int i=0; i<n; i++)
+		printf("%d ", fib(i));
+
+	printf("\n");
 	return 0;
 }

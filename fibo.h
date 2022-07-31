2fe1a5b9b11b89c08cf8d418ecf849a51af63032
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-07-31 21:44:33 +0530

    recursive fibonacci

--- fibo.h
+++ fibo.h
@@ -1,4 +1,23 @@
 #ifndef FIB_H
 #define FIB_H
 
+/* --- recursive definition of fibonacci series:
+ *           _
+ *          |
+ *          | 1,  n=0 or 1
+ *          /
+ * fib(n)= -
+ *          \ fib(n-1)+fib(n-2), n>1
+ *          |
+ *          |_
+ */
+
+int fib(int n)
+{
+	if(n==0 || n==1)
+		return 1;
+
+	return fib(n-1)+fib(n-2);
+}
+
 #endif

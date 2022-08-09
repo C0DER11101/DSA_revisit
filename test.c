dd610992ee3936538ecdc2bb6059984d3f805beb
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-08-09 19:58:55 +0530

    testing

---
+++ test.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a[3][3], b[3][3], c[3][3];
+
+
+	printf("enter elements into the matrix a:\n\n");
+
+	for(int i=0; i<3; i++)
+	{
+		for(int j=0; j<3; j++)
+		{
+			printf("[%d][%d]: ", i, j);
+			scanf("%d", &a[i][j]);
+		}
+
+	}
+
+	printf("enter elements into the matrix b:\n\n");
+
+	for(int i=0; i<3; i++)
+	{
+		for(int j=0; j<3; j++)
+		{
+			printf("[%d][%d]: ", i, j);
+			scanf("%d", &b[i][j]);
+		}
+
+	}
+
+	printf("\n\naxb = \n\n");
+
+
+	for(int i=0; i<3; i++)
+	{
+		for(int j=0; j<3; j++)
+		{
+			c[i][j]=0;
+			for(int k=0; k<3; k++)
+			{
+				c[i][j]+=a[i][k]*b[k][j];
+			}
+		}
+
+	}
+
+	for(int i=0; i<3; i++)
+	{
+		for(int j=0; j<3; j++)
+			printf("%d ", c[i][j]);
+		printf("\n");
+	}
+
+	return 0;
+}

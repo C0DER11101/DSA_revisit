550b84f482e5128fa85ab3f1387ef87450570b00
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-08-14 22:25:37 +0530

    just a header file that contains symbolic constants

--- maxm.h
+++ maxm.h
@@ -1,5 +1,11 @@
 #ifndef maxm_H
 #define maxm_H
 #define MAX 100
+#define TRUE 1
+#define FALSE 0
+#define INVALID -1
+#define and &&
+#define or ||
+#define not !
 // more symbolic constants awaiting to be defined!!
 #endif

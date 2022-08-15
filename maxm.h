8e6348e4c4e0466403887ab02ca23e86034c390b
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-08-15 20:13:06 +0530

    just a header file that contains symbolic constants

--- maxm.h
+++ maxm.h
@@ -12,9 +12,9 @@
 /* for graphs!!! */
 /* BFS */
 
-#define initial 0 // state 1 of vertex
-#define waiting 1 // state 2 of vertex
-#define visited 2 // state 3 of vertex
+#define initial 0 // initial state of vertex
+#define waiting 1 // waiting state of vertex
+#define visited 2 // visited state of vertex
 // more symbolic constants awaiting to be defined!!
 int state[MAX]; // the status of each vertex in bfs, whether it is in initial state or waiting state or visited state!!
 #endif

bc0c8ab727cf8f117f950051f5dd600cdfe0a8ce
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-08-15 20:03:00 +0530

    BREADTH FIRST SEARCH

---
+++ GraphMat.h
@@ -0,0 +1,46 @@
+#ifndef GRAPH_MAT
+#define GRAPH_MAT
+// HEADER FILE FOR ADJACENCY MATRIX!!!
+
+int adj[MAX][MAX];
+
+int edge(int intV, int dstV)
+{
+	if(intV<0 && dstV<0)
+	{
+		printf("\n\nSTOPPED!!\n\n");
+		return ERR;
+	}
+
+	else if(intV<0 || dstV<0)
+	{
+		printf("\n\ninvalid vertex!!\n\n");
+		return ERR;
+	}
+
+	else
+	{
+		adj[intV][dstV]=1;
+		return OK;
+	}
+
+}
+
+void showadj(int numVertices)
+{
+	printf("        v%d\t", 0);
+
+	for(int j=1; j<numVertices; j++)
+		printf("v%d\t", j);
+	printf("\n");
+
+	for(int i=0; i<numVertices; i++)
+	{
+		printf("v%d\t", i);
+		for(int j=0; j<numVertices; j++)
+			printf("%d\t", adj[i][j]);
+		printf("\n");
+	}
+}
+
+#endif

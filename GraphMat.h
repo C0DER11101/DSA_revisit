1470ffbc38dfd3c4d6a4db0f553b12d4f962c3bf
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-08-17 20:56:24 +0530

    Header file for BFS(digraph and undirected grah)

--- GraphMat.h
+++ GraphMat.h
@@ -5,6 +5,7 @@
 
 int adj[MAX][MAX];
 
+#if TYPE==1 // 1 is for directed graph (TYPE is defined in "maxm.h")
 int edge(int intV, int dstV)
 {
 	if(intV<0 && dstV<0)
@@ -27,18 +28,42 @@ int edge(int intV, int dstV)
 
 }
 
-void showadj(int numVertices)
+#else
+
+void edge(int iV, int tV)
+{
+	if(iV<0 && tV<0)
+	{
+		printf("\n\nSTOPPED!!!\n\n");
+		return;
+	}
+
+	else if(iV<0 || tV<0)
+	{
+		printf("invalid vertices!!\n\n");
+		return;
+	}
+
+	else
+	{
+		adj[iV][tV]=1;
+		adj[tV][iV]=1;
+	}
+}
+#endif
+
+void showadj(int numV)
 {
 	printf("        v%d\t", 0);
 
-	for(int j=1; j<numVertices; j++)
+	for(int j=1; j<numV; j++)
 		printf("v%d\t", j);
 	printf("\n");
 
-	for(int i=0; i<numVertices; i++)
+	for(int i=0; i<numV; i++)
 	{
 		printf("v%d\t", i);
-		for(int j=0; j<numVertices; j++)
+		for(int j=0; j<numV; j++)
 			printf("%d\t", adj[i][j]);
 		printf("\n");
 	}

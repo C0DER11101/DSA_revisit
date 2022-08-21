623d22ca25d19be581fa366b83a4d39667573406
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-08-21 22:50:02 +0530

    contains symbolic constants

--- maxm.h
+++ maxm.h
@@ -25,11 +25,13 @@
 #define waiting 1 // waiting state of vertex
 #define visited 2 // visited state of vertex
 // more symbolic constants awaiting to be defined
-int state[MAX]={initial}; // the status of each vertex in bfs, whether it is in initial state or waiting state or visited state
+int state[MAX]={initial}; // the status of each vertex in bfs(and dfs), whether it is in initial state or waiting state or visited state
+/* in dfs, the vertex is either  in initial state or visited state(it has no waiting state)*/
 int pred[MAX]; // predecessor of a vertex -> BFS for finding the shortest path, also for DFS
 int dist[MAX]; // distance value of a vertex -> BFS for finding the shortest path
 int SPath[MAX]; // stores the shortest path from starting vertex to end vertex
 int Spidx; // keeps track of number of vertices in SPath that form the shortest path
 int edgeCount; // for eounting number of edges!!
+int status; // status of edge() in GraphMat.h
 
 #endif

111d7d67446fb4edd9695d8dc34a4eda0a9ac2e8
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-08-22 20:26:43 +0530

    Depth First Search

--- dfs.h
+++ dfs.h
@@ -4,86 +4,139 @@
 #if TYPE==1 // 1 is for directed graphs
 void dfs(int startV, int numV)
 {
-    push(startV);
 
-    pred[startV]=-1;
+	while(state[startV]==initial)
+	{
+		push(startV);
+		pred[startV]=-1;
 
-    while(Top()!=-1)
-    {
-        startV=pop();
-        if(state[startV]==initial)
-            printf("%d ", startV);
-        state[startV]=visited;
+		while(Top()!=-1)
+		{
+			startV=pop();
+			if(state[startV]==initial)
+				printf("%d ", startV);
+			state[startV]=visited;
 
-        for(int i=numV-1; i>=0; i--)
-        {
-            if(adj[startV][i]==1 && state[i]==initial)
-            {
-                push(i);
-                pred[i]=startV;
-            }
-        }
-    }
+			for(int i=numV-1; i>=0; i--)
+			{
+				if(adj[startV][i]==1 && state[i]==initial)
+				{
+					push(i);
+					pred[i]=startV;
+				}
+			}
+		}
 
-    // restoring the states of the vertices
+		for(int i=0; i<numV; i++)
+		{
+			if(state[i]==initial)
+			{
+				startV=i;
+				break;
+			}
+		}
+	}
 
-    for(int i=0; i<numV; i++)
-        state[i]=initial;
+	// restoring the states of the vertices
+
+	for(int i=0; i<numV; i++)
+		state[i]=initial;
 
 }
 
 void spanningTreeEdge(int numV)
 {
-    for(int i=0; i<numV; i++)
-    {
-        if(pred[i]!=-1)
-            printf("%d->%d\n", pred[i], i);
-    }
+	for(int i=0; i<numV; i++)
+	{
+		if(pred[i]!=-1)
+			printf("%d->%d\n", pred[i], i);
+	}
 }
 
 void backEdge(int numV)
 {
-    for(int i=numV-1; i>=0; i--)
-    {
-        for(int j=i; pred[j]!=-1; j=pred[j])
-        {
-            if(adj[i][pred[j]]==1 && j!=i)
-                printf("%d->%d\n", pred[j], i);
-        }
-    }
+	for(int i=numV-1; i>=0; i--)
+	{
+		for(int j=i; pred[j]!=-1; j=pred[j])
+		{
+			if(adj[i][pred[j]]==1 && j!=i)
+				printf("%d->%d\n", i, pred[j]);
+		}
+	}
 }
 
 void forwardEdge(int numV)
 {
-    for(int i=0; i<numV; i++)
-    {
-        for(int j=i; pred[j]!=-1; j=pred[j])
-        {
-            if(adj[pred[j]][i]==1 && j!=i)
-                printf("%d->%d\n", pred[j], i);
-        }
-    }
+	for(int i=0; i<numV; i++)
+	{
+		for(int j=i; pred[j]!=-1; j=pred[j])
+		{
+			if(adj[pred[j]][i]==1 && j!=i)
+				printf("%d->%d\n", pred[j], i);
+		}
+	}
 }
 
-#else
+void crossEdge(int numV)
+{
+	bool isValid=true;
+
+	for(int i=0; i<numV; i++)
+	{
+		for(int j=0; j<numV; j++)
+		{
+			if(adj[i][j]==1)
+			{
+				for(int k=pred[i]; k!=-1; k=pred[k]) // predecessor list of i
+				{
+					if(j==k) // j falls in the predecessor list of i then i and j cannot form cross edge!
+					{
+						isValid=false;
+						break;
+					}
+				}
+
+				if(isValid==true)
+				{
+					for(int k=pred[j]; k!=-1; k=pred[k]) // if i falls in the predecessor list of j then they can't form cross edge!
+					{
+						if(i==k)
+						{
+							isValid=false;
+							break;
+						}
+					}
+				}
+
+				if(isValid==true)
+					printf("%d->%d\n", i, j);
+				else
+					isValid=true;
+			}
+		}
+	}
+}
+
+
+#else // for undirected graphs!!
 
 void dfs(int startV, int numV)
 {
-    push(startV);
+	push(startV);
 
-    while(Top()!=-1)
-    {
-        startV=pop();
-        if(state[startV]==initial)
-            printf("%d ", startV);
-        state[startV]=visited;
+	while(Top()!=-1)
+	{
+		startV=pop();
+		if(state[startV]==initial)
+			printf("%d ", startV);
+		state[startV]=visited;
 
-        for(int i=numV-1; i>=0; i--)
-        {
-            if(adj[startV][i]==1 && state[i]==initial)
-                push(i);
-        }
-    }
+		for(int i=numV-1; i>=0; i--)
+		{
+			if(adj[startV][i]==1 && state[i]==initial)
+				push(i);
+		}
+	}
 }
 
 #endif // TYPE

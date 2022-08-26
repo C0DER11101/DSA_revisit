91f1b5abd64353237b2fc918ee5175d168d6ef38
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-08-26 23:37:04 +0530

    Graph Functions

--- GraphMat.h
+++ GraphMat.h
@@ -5,50 +5,56 @@
 
 int adj[MAX][MAX];
 
+void restoreState(int numV) // restore the states of each vertex!!!
+{
+	for(int i=0; i<numV; i++)
+		state[i]=initial;
+}
+
 #if TYPE==1 // 1 is for directed graph (TYPE is defined in "maxm.h")
 int edge(int iV, int tV)
 {
-        if(iV<0 || tV<0)
-                return ERR;
-        
-        else
-                adj[iV][tV]=1;
-        
-        return OK;
+	if(iV<0 || tV<0)
+		return ERR;
+
+	else
+		adj[iV][tV]=1;
+
+	return OK;
 }
 
 #else
 
 int edge(int iV, int tV)
 {
-        if(iV<0 || tV<0)
-                return ERR;
-        
-        else
-        {
-                adj[iV][tV]=1;
-                adj[tV][iV]=1;
-        }
-        
-        return OK;
+	if(iV<0 || tV<0)
+		return ERR;
+
+	else
+	{
+		adj[iV][tV]=1;
+		adj[tV][iV]=1;
+	}
+
+	return OK;
 }
 #endif
 
 void showadj(int numV)
 {
-        printf("        v%d\t", 0);
-        
-        for(int j=1; j<numV; j++)
-                printf("v%d\t", j);
-        printf("\n");
-        
-        for(int i=0; i<numV; i++)
-        {
-                printf("v%d\t", i);
-                for(int j=0; j<numV; j++)
-                        printf("%d\t", adj[i][j]);
-                printf("\n");
-        }
+	printf("        v%d\t", 0);
+
+	for(int j=1; j<numV; j++)
+		printf("v%d\t", j);
+	printf("\n");
+
+	for(int i=0; i<numV; i++)
+	{
+		printf("v%d\t", i);
+		for(int j=0; j<numV; j++)
+			printf("%d\t", adj[i][j]);
+		printf("\n");
+	}
 }
 
 #endif

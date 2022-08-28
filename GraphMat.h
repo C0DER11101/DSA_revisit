9816cc618f1893972455c4756aa1ec3d796703a5
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-08-28 22:33:55 +0530

    Graph Functions

--- GraphMat.h
+++ GraphMat.h
@@ -1,9 +1,6 @@
-#ifndef GRAPH_MAT
+#ifndef GRAPH_MAT // Graph Materials!!
 #define GRAPH_MAT
 
-// HEADER FILE FOR ADJACENCY MATRIX!!!
-
-int adj[MAX][MAX];
 
 void restoreState(int numV) // restore the states of each vertex!!!
 {
@@ -22,6 +19,33 @@ int edge(int iV, int tV)
 
 	return OK;
 }
+
+void GetWeight(int iV, int tV)
+{
+	int w;
+enterWeight:
+	printf("\nenter weight of the edge(%d,%d): ", iV, tV);
+	scanf("%d", &w);
+
+#if DJKAL == 1
+	if(weight<0)
+	{
+		printf("\n\nDijkstra's algorithm doesnot work on negative weights!!\n\n");
+		goto enterWeight;
+	}
+
+	else
+	{
+		weight[iV][tV]=w;
+	}
+
+#else
+	weight[iV][tV]=w;
+
+#endif
+
+}
+
 
 #else
 
@@ -55,6 +79,29 @@ void showadj(int numV)
 			printf("%d\t", adj[i][j]);
 		printf("\n");
 	}
+}
+
+
+void showWeight(int numV)
+{
+	printf("        v%d\t", 0);
+
+	for(int j=1; j<numV; j++)
+		printf("v%d\t", j);
+	printf("\n");
+
+	for(int i=0; i<numV; i++)
+	{
+		printf("v%d\t", i);
+		for(int j=0; j<numV; j++)
+			printf("%d\t", weight[i][j]);
+		printf("\n");
+	}
+}
+
+int Weight(int iV, int tV)
+{
+	return weight[iV][tV];
 }
 
 #endif

55469982d00e2f7b2ea570487ff9d6535fb44380
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-07 23:24:50 +0530

    Bubble sort

---
+++ BubbleSort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include "Array.h"
+
+
+int main(void)
+{
+	int size;
+
+	printf("enter number of elements: ");
+	scanf("%d", &size);
+
+	printf("enter elements:\n\n");
+
+	for(int i=0; i<size; i++)
+	{
+		printf("element %d: ", i+1);
+		scanf("%d", arr+i);
+	}
+
+	Bsort(size);
+	
+	printf("\n\nSorted array:\n\n");
+
+	for(int i=0; i<size; i++)
+		printf("%d ", arr[i]);
+
+
+	printf("\n");
+	return 0;
+}

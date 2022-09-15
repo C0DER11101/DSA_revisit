b9bdac5cc3753ebfe4b34cad306f999c1ab768ed
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-15 22:09:55 +0530

    compile c and c++ programs

--- cm.sh
+++ cm.sh
@@ -1,10 +1,22 @@
 #compile c programs
 
+thirdArg=0 # in case you need to compile a c++ file
+
 if [ -e $1 ]
 then
 	if [ -f $1 ]
 	then
-		gcc $1 -o $2
+		if [ $3 == "c" ]
+		then
+			gcc $1 -o $2
+		elif [ $3 == "cpp" ]
+		then
+			g++ $1 -o $2
+
+		elif [ $3 == "nil" ]
+		then
+			echo "invalid third argument!!"
+		fi
 
 	else
 		echo "$1 is not a file!!"

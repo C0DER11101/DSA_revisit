74bc102f847f86feab593209a56e692364fe8ad4
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-21 19:48:16 +0530

    Test program in c++

--- test.cpp
+++ test.cpp
@@ -7,20 +7,18 @@ using namespace std;
 char**s;
 int main(void)
 {
-	int row, col;
+	int row;
 
 	cout<<"enter number of rows: ";
 	cin>>row;
 
-	cout<<"enter number of columns: ";
-	cin>>col;
-	getchar(); // since cin leaves a newline character, it must be stored in, otherwise it will hamper the inputs
+	getchar(); // since cin leaves a newline character, it must be stored, otherwise it will hamper the inputs
 	// that we are going to take using cin.getline() function
 
 	s=new char*[row]; // allocating memory for row number of pointers to integer!!
 
-	for(int i=0; i<col; i++)
-		s[i]=new char[col]; // each pointer is allocated memory for an array of characters that has a length of col!!
+	for(int i=0; i<row; i++)
+		s[i]=new char[10]; // each pointer is allocated memory for an array of characters that has a length of col!!
 
 
 	cout<<"enter strings:\n\n";
@@ -34,7 +32,7 @@ int main(void)
 	for(int i=0; i<row; i++)
 	{
 		cout<<"String "<<i+1<<"\n";
-		for(int j=0; j<col; j++)
+		for(int j=0; j<10; j++)
 		{
 			if(s[i][j]==' ')
 				;

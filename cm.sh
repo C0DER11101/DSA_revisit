a80084c42c8a172de9971b69a6f676d0b077e8ed
Priyanuj Bora <while.alive.program@gmail.com>
2022-11-14 07:02:08 +0530

    sh file incompletegit add cm.sh

--- cm.sh
+++ cm.sh
@@ -2,31 +2,10 @@
 
 if [ -e $1 ]
 then
-	if [ -f $1 ]
-	then
-		while read p
-		do
-			if [ $p=="iostream" ]
-			then
-				echo "G++"
-				g++ $1 -o $2
-				break
-
-			elif [ $p=="stdio.h" ]
-			then
-				echo "GCC"
-				gcc $1 -o $2
-				break
-
-			fi
-
-		done < $1
-	else
-		echo "$1 is not a file!!"
-
-	fi
-
-else
-	echo "$1 doesnot exist!!"
-
-fi
+	case ${1##*.} in
+		"c"))
+			while read p
+			do
+				if [ $p == "math.h" ] -o [ $p == "cmath" ]
+				then
+					# code ;
